22102a425f385dddc0439aeb0e7657a04d445cb3
Vlasko <luka.j.v@gmail.com>
2019-12-18 17:46:15 +0000

    Add time upload functionality

--- processing.py
+++ processing.py
@@ -1,5 +1,6 @@
 # Standard Library Imports
 import os
+from time import sleep
 
 #Third Party Imports
 from datetime import datetime
@@ -12,7 +13,7 @@ from functions.price_functions import price_store
 
 #call(['node', './tplink-energy-monitor/app.js'])
 
-def upload_download():
+def upload_demand():
     directory = os.getcwd()
     price_store()
     print('price stored')
@@ -25,3 +26,9 @@ def upload_download():
     current_demand_json_to_csv(directory+'/files/demand/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json')
     upload_to_aws(directory+'/files/demand/'+ today +'_demand.csv',
                   'fridgedemand', today +'_demand.csv')
+
+condition = False
+while condition == False:
+    if datetime.now().hour == 23 and datetime.now().minute == 59:
+        upload_demand()
+        sleep(60)
