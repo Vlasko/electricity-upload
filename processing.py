abbe858ab4297f2268975f93340d2023c791f13b
Vlasko <luka.j.v@gmail.com>
2019-12-18 11:42:52 +0000

    Add processing file

---
+++ processing.py
@@ -0,0 +1,30 @@
+# Standard Library Imports
+import os
+import sys
+
+sys.path
+
+#Third Party Imports
+from datetime import datetime
+# from subprocess import call
+
+# Local Application Imports
+from functions.demand_functions import current_demand_json_to_csv
+from functions.aws_functions import upload_to_aws
+from functions.price_functions import price_store
+
+#call(['node', './tplink-energy-monitor/app.js'])
+
+def upload_download():
+    directory = os.getcwd()
+    price_store()
+    print('price stored')
+    today =  datetime.now().date().strftime('%Y-%m-%d')
+
+    # Prices
+    upload_to_aws(directory+'/files/prices/'+ today +'_prices.csv',
+                  'lukaprices', today +'_prices.csv')
+    # Demand
+    current_demand_json_to_csv(directory+'/files/demand/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json')
+    upload_to_aws(directory+'/files/demand/'+ today +'_demand.csv',
+                  'fridgedemand', today +'_demand.csv')
