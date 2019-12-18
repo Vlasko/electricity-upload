0dfeb500e3ec870313399712906e6d36cc1ec5da
Vlasko <luka.j.v@gmail.com>
2019-12-18 17:43:36 +0000

    change AWS keys

--- functions/aws_functions.py
+++ functions/aws_functions.py
@@ -1,15 +1,11 @@
 # Standard Library Imports
-import os
-import sys
 
 # Third Party Imports
 import boto3
 from botocore.exceptions import NoCredentialsError
 
 # Local Application Imports
-path_prefix = os.path.split(os.getcwd())[0]
-sys.path.append(path_prefix+'/Fridge_Flex/Keys')
-from AWS_keys import ACCESS_KEY, SECRET_KEY
+from functions.AWS_keys import ACCESS_KEY, SECRET_KEY
 
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
