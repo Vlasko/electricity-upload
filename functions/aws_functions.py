0a656db72c31d8e1072bc6bfbe4652894f8d458c
Vlasko <luka.j.v@gmail.com>
2019-12-19 11:31:58 +0000

    import whole boto3 module

--- functions/aws_functions.py
+++ functions/aws_functions.py
@@ -1,14 +1,14 @@
 # Standard Library Imports
 
 # Third Party Imports
-from boto3 import client
+import boto3
 from botocore.exceptions import NoCredentialsError
 
 # Local Application Imports
 from keys.AWS_keys import ACCESS_KEY, SECRET_KEY
 
 def upload_to_aws(local_file, bucket, s3_file):
-    s3 = client('s3', aws_access_key_id=ACCESS_KEY,
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
 
     try:
