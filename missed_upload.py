1f32119642003018997a802e52e0dc2751f685b2
Vlasko <luka.j.v@gmail.com>
2020-04-08 10:11:24 +0100

    Allow for input of any date

--- missed_upload.py
+++ missed_upload.py
@@ -10,4 +10,5 @@ from functions.demand_functions import generic_demand_json_to_csv
 directory = os.getcwd()
 json_path = '/tplink-energy-monitor/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json'
 
-generic_demand_json_to_csv(json_path, '2020-04-07')
+date_missed= input('What date was missed')
+generic_demand_json_to_csv(json_path, date_missed)
