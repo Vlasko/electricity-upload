36fa760fd364182263b2e4eb41fba37deb5a246d
Vlasko <luka.j.v@gmail.com>
2020-04-08 10:05:39 +0100

    Added new file for missed upload

---
+++ missed_upload.py
@@ -0,0 +1,13 @@
+# Standard Library Imports
+import os
+
+# Third Party Imports
+
+
+# Local Application Imports
+from functions.demand_functions import generic_demand_json_to_csv
+
+directory = os.getcwd()
+json_path = directory + '/tplink-energy-monitor/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json'
+
+generic_demand_json_to_csv(json_path, '2020-04-07')
