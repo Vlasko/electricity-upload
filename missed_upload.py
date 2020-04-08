eca88657a46d0b9097509384b9fae1fdc6a9260b
Vlasko <luka.j.v@gmail.com>
2020-04-08 10:06:51 +0100

    Fix path issues

--- missed_upload.py
+++ missed_upload.py
@@ -8,6 +8,6 @@ import os
 from functions.demand_functions import generic_demand_json_to_csv
 
 directory = os.getcwd()
-json_path = directory + '/tplink-energy-monitor/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json'
+json_path = '/tplink-energy-monitor/8006AD6134CA39AE8FD966D4E1A709821B20C760-log.json'
 
 generic_demand_json_to_csv(json_path, '2020-04-07')
